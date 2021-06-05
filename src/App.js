7d4f6a4e0d919094152b823d243416154653986d
UpcomingDeveloper <UpcomingDeveloper>
2021-06-05 17:05:52 +0530

    firs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect,useState} from 'react'
+const  Covid=()=>{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [data, setData] = useState([]);
+
+  const getCovidDate= async()=>{
+  
+    try {
+      const res= await fetch('https://api.covid19india.org/data.json');
+     const actualData=await res.json();
+     setData(actualData.statewise[0]);
+    }
+    catch(err){
+     console.log(err); 
+    }
+  
+  }
+  useEffect(() => {
+    getCovidDate();
+    
+  }, []);
+
+  return(
+   
+    <>
+    <section >
+    <h1 >🔴 LIVE</h1>
+    <h2>COVID-19 CORONAVIRUS TRACKER</h2>
+    <ul >
+      <li className="card">
+        <div className="card_main">
+          <div className="card_inner">
+            <p className="card_back">Our <span>Country</span></p>
+            <p className="card_total"><span>INDIA</span></p>
+            </div>
+        </div>
+      </li>
+    </ul>
+    <ul >
+      <li className="card">
+        <div className="card_main">
+          <div className="card_inner">
+            <p className="card_back">Total <span>RECOVERD</span></p>
+            <p className="card_total">{data.recovered}</p>
+            </div>
+        </div>
+      </li>
+    </ul>
+    <ul >
+      <li className="card">
+        <div className="card_main">
+          <div className="card_inner">
+            <p className="card_back"> <span>CONFIRMED</span></p>
+            <p className="card_total">{data.confirmed}</p>
+            </div>
+        </div>
+      </li>
+    </ul>
+    <ul >
+      <li className="card">
+        <div className="card_main">
+          <div className="card_inner">
+            <p className="card_back">Total <span>DEATH</span></p>
+            <p className="card_total">{data.deaths}</p>
+            </div>
+        </div>
+      </li>
+    </ul>
+    <ul >
+      <li className="card">
+        <div className="card_main">
+          <div className="card_inner">
+            <p className="card_back">Total <span>ACTIVE</span></p>
+            <p className="card_total">{data.active}</p>
+            </div>
+        </div>
+      </li>
+    </ul>
+    <ul >
+      <li className="card">
+        <div className="card_main">
+          <div className="card_inner">
+            <p className="card_back">Last <span>UPDATED</span></p>
+            <p className="card_total">{data.lastupdatedtime}</p>
+            </div>
+        </div>
+      </li>
+    </ul>
+     
+    </section>
+    <div className="footer">
+        <p>By Shivani Verma</p>
+      </div>
+    </>
+  )
 }
-
-export default App;
+export default Covid;
